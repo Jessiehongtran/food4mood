79c37d388749b297f1ff9490cbfbe82e7d493241
Hong Tran <jessietran@fb.com>
2022-01-05 08:41:17 -0800

    done menu

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -6,13 +6,18 @@ const Menu = () => {
     const [emojiList, setEmojiList] = useState(emoji)
 
     const showEmoji = (id: number) => {
-        
-        
+        const copiedEmojiList = emojiList
+        copiedEmojiList.forEach(each => each.id === id ? each.show = true : false)
+        setEmojiList([...copiedEmojiList])
     }
 
     const hideEmoji = (id: number) => {
-       
+        const copiedEmojiList = emojiList
+        copiedEmojiList.forEach(each => each.show = false)
+        setEmojiList([...copiedEmojiList])
     }
+
+    console.log('out', emojiList)
 
 
     return (
@@ -25,7 +30,9 @@ const Menu = () => {
                     onMouseLeave={() => hideEmoji(each.id)}
                     style={{ width: '50px', margin: '5px', backgroundColor: '#F6F5F5', borderRadius: '50%' }} 
                 />
-                <div style={{ backgroundColor: '#000', color: '#fff', padding: '5px 8px', borderRadius: '4px', position: 'absolute', top: '-20px', left: '35px' }}>{each.name}</div>
+                {each.show? 
+                    <div style={{ backgroundColor: '#000', color: '#fff', padding: '5px 8px', borderRadius: '4px', position: 'absolute', top: '-20px', left: '35px' }}>{each.name}</div>
+                    : null}
             </div>
             )}
         </div>
